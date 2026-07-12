04dbb66c8af7a3309370a90784ae6caffe4e7689
PriceyNP20 <ianprice1980@msn.com>
2026-07-12 17:51:34 +0100

    Install shared table colour rules

--- src/ian_racing_model/__init__.py
+++ src/ian_racing_model/__init__.py
@@ -3,3 +3,12 @@
 __all__ = ["__version__"]
 
 __version__ = "0.1.0"
+
+try:
+    from . import ui as _ui
+    from .table_styles import install_streamlit_table_styles, picks_tracker_style as _picks_tracker_style
+
+    _ui.picks_tracker_style = _picks_tracker_style
+    install_streamlit_table_styles()
+except Exception:
+    pass
