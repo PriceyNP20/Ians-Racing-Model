62e3a764a73e46268945bd9bf29612d0c2624321
PriceyNP20 <ianprice1980@msn.com>
2026-07-12 19:14:06 +0100

    Install calibration rules at startup

--- src/ian_racing_model/__init__.py
+++ src/ian_racing_model/__init__.py
@@ -6,8 +6,10 @@ __version__ = "0.1.0"
 
 try:
     from . import ui as _ui
+    from .calibration_rules import install_calibration_rules
     from .table_styles import install_streamlit_table_styles, picks_tracker_style as _picks_tracker_style
 
+    install_calibration_rules(_ui)
     _ui.picks_tracker_style = _picks_tracker_style
     install_streamlit_table_styles()
 except Exception:
